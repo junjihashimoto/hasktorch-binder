2ebf6dfde124e9a9d641b2f0f752f36720821ad9
Junji Hashimoto <junji.hashimoto@gree.net>
2025-07-19 14:45:49 +0900

    Update

--- default.nix
+++ default.nix
@@ -23,28 +23,11 @@ pkgs.mkShell {
     cabal-install
     stack
     ihaskell
-    libtorch-bin
     
     # Python and Jupyter for mybinder
-    python3Packages.jupyter
-    python3Packages.notebook
+    python3Packages.jupyterlab
     
     # Additional tools
     git
   ];
-
-  shellHook = ''
-    echo "Hasktorch Binder Environment"
-    echo "============================"
-    echo "GHC version: $(ghc --version)"
-    echo "Cabal version: $(cabal --version | head -n1)"
-    echo ""
-    echo "To start Jupyter with IHaskell:"
-    echo "  jupyter notebook"
-    echo ""
-    
-    # Export library paths for libtorch
-    export LD_LIBRARY_PATH=${pkgs.libtorch-bin}/lib:$LD_LIBRARY_PATH
-    export LIBTORCH=${pkgs.libtorch-bin}
-  '';
 }
